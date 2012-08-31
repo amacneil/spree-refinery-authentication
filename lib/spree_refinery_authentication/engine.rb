744316248107e9b83f26099aa2d8b4e351a9bfad
Adrian Macneil <adrian@crescendo.net.nz>
2012-08-31 12:27:59 +1000

    Fix WillPaginate v Kaminari bug

--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -4,11 +4,12 @@ module SpreeRefineryAuthentication
   class Engine < Rails::Engine
     engine_name "spree_refinery_authentication"
 
-    config.before_initialize do
-      Spree.user_class = "Refinery::User"
-    end
-
     config.to_prepare do
+      Spree.user_class = "Refinery::User"
+
+      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :per, :per_page
+      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :num_pages, :total_pages
+
       ApplicationController.send :include, Spree::AuthenticationHelpers
     end  
   end
